c280709966cb869feb4db6a6000cf69a97b9df11
Karsten Heymann <karsten.heymann@gmail.com>
2019-09-27 15:43:20 +0200

    Fix haproxy.cfg comment in spec test

--- spec/classes/haproxy_spec.rb
+++ spec/classes/haproxy_spec.rb
@@ -101,7 +101,7 @@ describe 'haproxy', type: :class do
           subject.should contain_concat__fragment('haproxy-00-header').with(
             'target'  => '/etc/haproxy/haproxy.cfg',
             'order'   => '01',
-            'content' => "# This file managed by Puppet\n",
+            'content' => "# This file is managed by Puppet\n",
           )
         end
         it 'contains a haproxy-haproxy-base concat fragment' do
